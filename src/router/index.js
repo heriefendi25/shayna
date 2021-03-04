c0511e77f2b2ea219eaac2690d5449b3f334ed35
Heri Efendi <heriefendi25@gmail.com>
2021-03-04 11:04:07 +0700

    upload shayna vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,39 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Product from "../views/Product.vue"
+import ShoppingCart from '../views/ShoppingCart.vue'
+import Success  from '../views/SuccessShayna.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    name: "Home",
+    component: Home,
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+    path: "/about",
+    name: "About",
+    component: () => import("../views/About.vue"),
+  },
+  {
+    path: "/product",
+    name: "Product",
+    component: Product,
+  },
+  {
+    path: "/cart",
+    name: "cart",
+    component: ShoppingCart
+  },
+  {
+    path: "/success",
+    name: "success",
+    component: Success
+  },
+];
 
 const router = new VueRouter({
   routes
